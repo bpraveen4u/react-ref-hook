84f375baa106d425723bb52b9029b9ed14c95f66
Praveen Kumar Bonakurthi <prbona@microsoft.com>
2020-04-23 10:08:50 +0530

    the ref hook demo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import FocusInput from './components/FocusInput'
+import ClassTimer from './components/ClassTimer'
+import HookTimer from './components/HookTimer'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <HookTimer />
+      <ClassTimer />
+      {/* <FocusInput /> */}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
